23747cc0ccf01a073e3c1dae4ac232e7ab209fbe
noman2016 <hafezalnoman@gmail.com>
2022-08-31 12:08:01 +0600

    Update all Files

--- BasicSeleniumUsingPython/FirstAutomationTestInBrowsers/FirstAutomationTestForChrome.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome(executable_path="E:\\SeleniumAutomation\\PythonProjects\\browserdrivers\\chromedriver.exe")
-
-driver.get("https://google.com")
-driver.maximize_window()
-print(driver.title)
-driver.close()
--- BasicSeleniumUsingPython/FirstAutomationTestInBrowsersDM/FirstAutomationTestForFirefoxDM.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-from webdriver_manager.firefox import GeckoDriverManager
-driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-
-driver.get("https://google.com")
-driver.maximize_window()
-print(driver.title)
-driver.close()
