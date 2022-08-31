775febcb0107579c7f2cafe2af7db949765283a9
noman2016 <hafezalnoman@gmail.com>
2022-08-31 12:15:22 +0600

    All Browsers are updated

--- FirstAutomationTestInBrowsers/FirstAutomationTestForEdge.py
+++ FirstAutomationTestInBrowsers/FirstAutomationTestForEdge.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-driver = webdriver.Edge(executable_path="E:\\SeleniumAutomation\\PythonProjects\\browserdrivers\\msedgedriver.exe")
+driver = webdriver.Edge(executable_path="E:\\SeleniumAutomation\\Python\\PythonSeleniumProjects\\BasicSeleniumUsingPython\\allbrowsers\\msedgedriver.exe")
 
 driver.get("https://google.com")
 driver.maximize_window()
--- FirstAutomationTestInBrowsers/FirstAutomationTestForFirefox.py
+++ FirstAutomationTestInBrowsers/FirstAutomationTestForFirefox.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-driver = webdriver.Firefox(executable_path="E:\\SeleniumAutomation\\PythonProjects\\browserdrivers\\geckodriver.exe")
+driver = webdriver.Firefox(executable_path="E:\\SeleniumAutomation\\Python\\PythonSeleniumProjects\\BasicSeleniumUsingPython\\allbrowsers\\geckodriver.exe")
 
 driver.get("https://google.com")
 driver.maximize_window()
